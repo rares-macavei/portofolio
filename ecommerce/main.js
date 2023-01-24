93a360945e4c2e40267444f64aaf073675a88050
Rares Macavei <raresmacavei15@gmail.com>
2023-01-24 18:19:40 +0200

    v1.0.3

---
+++ ecommerce/main.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+window.addEventListener("load", () => {
+    const loader = document.querySelector('.loader');
+    const content = document.querySelector('.content-loading');
+    let counter = 0;
+    setInterval(() =>{
+        if(counter == 15) {
+            loader.classList.add("loader-hidden");
+            content.classList.remove("content-loading");
+            loader.addEventListener("transitionend", () => {
+                document.body.removeChild("loader");
+            })
+        } else {
+            counter += 1;
+        }
+    }, 80);
+});
